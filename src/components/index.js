6396d4926605ab1d4829c7d3bbd73fd1efc4f549
Ishan Chaturvedi <ishanchaturvedi25@gmail.com>
2024-11-11 13:33:09 +0530

    Added new components to index.js and configured routing in main.jsx.

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,29 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
+import Button from './Button';
+import Input from './Input';
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Select from './Select';
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from './Login';
+import PostCard from './PostCard';
+import PostForm from "./post-form/PostForm";
+import AuthLayout from './AuthLayout';
 
 export {
     Header,
     Footer,
     Container,
+    Button,
+    Input,
     Logo,
     LogoutBtn,
-    RTE
+    Select,
+    RTE,
+    Signup,
+    PostCard,
+    PostForm
 }
